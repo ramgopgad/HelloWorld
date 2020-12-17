7ab11a11fa495936de2622414ddd6b86db6c52f3
ramgopgad <ramgopal.gadde@gmail.com>
2020-12-17 18:17:45 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ owners = ["amazon"] # Canonical
 }
 
 resource "aws_instance" "prod" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = data.aws_ami.prod_ami.id
   instance_type = "t3.micro"
 
   tags = {
