233b918c0d787a31e776b3632a346d02e9ea1dc3
ramgopgad <ramgopal.gadde@gmail.com>
2020-12-17 18:41:01 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,14 +18,21 @@ data "aws_ami" "prod_ami" {
     name   = "name"
       values = ["amzn2-ami-hvm-2.0.20201126.0-x86_64-gp2"]
   }
-
+  
+  data "key_pair" "rams_key" {
+  filter {
+    name   = "name"
+      values = ["MyUSE1KP"]
+  }
+   
 owners = ["amazon"] # Canonical
 }
 
 resource "aws_instance" "prod" {
   ami           = data.aws_ami.prod_ami.id
   instance_type = "t3.micro"
-
+  key_name      = aws_key_pair.rams_key.key_name
+  
   tags = {
     Name = "HelloWorld"
   }
