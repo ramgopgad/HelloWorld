58b5012f782adbeb15cf417cb0cda5ac9d7bc6a0
ramgopgad <ramgopal.gadde@gmail.com>
2020-12-17 18:53:21 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ owners = ["amazon"] # Canonical
 resource "aws_instance" "prod" {
   ami           = data.aws_ami.prod_ami.id
   instance_type = "t3.micro"
-//  key_name      = aws_key_pair.rams_key.key_name
+/  key_name      = "MyUSE1KP"
   
   tags = {
     Name = "HelloWorld"
